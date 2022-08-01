5633ee42b345494a07cf5aab49dfad0b1947f5b5
damirdine <damirdine@outlook.fr>
2022-08-01 11:50:10 +0200

    test

--- src/classes/User.php
+++ src/classes/User.php
@@ -1,15 +1,3 @@
 <?php
 require_once('../src/pdo/pdo.php');
 
-
-class User{
-    private $id;
-    private $afpaId;
-    private $email;
-    private $firstname;
-    private $lastname;
-
-    public function getEmail(){
-        return $this->id;
-    }
-}
