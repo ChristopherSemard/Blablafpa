a8801abeaaaba0d11f946db188617cdb3e70418e
ChristopherSemard <christophersemard76@gmail.com>
2022-08-01 17:31:43 +0200

    fix inscription

--- src/controllers/Signup.php
+++ src/controllers/Signup.php
@@ -10,53 +10,80 @@ function displayFormSignup(){
 function submitSignup($input){
     require_once('../src/pdo/pdo.php');
     session_start();
-if(isset($input['afpaId']) && isset($input['email']) && isset($input['firstName']) && isset($input['lastName']) && isset($input['password']) && isset($input['password_retype'])){
-    var_dump($input);
-    $idAfpa = htmlspecialchars($input['afpaId']);
-    $firstName = htmlspecialchars($input['firstName']);
-    $lastName = htmlspecialchars($input['lastName']);
-    $email = htmlspecialchars($input['email']);
-    $password = htmlspecialchars($input['password']);
-    $password_retype = htmlspecialchars($input['password_retype']);
-    
 
-    $check = $bdd->prepare('SELECT afpa_id, firstname, lastname, email, password FROM users WHERE afpa_id = ?');
-    
-    $check->execute(array($idAfpa));
-    $data = $check->fetch();
-    $row = $check->rowCount();
-var_dump($data);
-    $email = strtolower($email); // on transforme toute les lettres majuscule en minuscule pour éviter que Foo@gmail.com et foo@gmail.com soient deux compte différents ..
+    if(isset($input['afpaId']) && isset($input['email']) && isset($input['firstName']) && isset($input['lastName']) && isset($input['password']) && isset($input['password_retype'])){
 
-    // Si la requete renvoie un 0 alors l'utilisateur n'existe pas 
-    if($row == 0){ 
-        if(strlen($idAfpa) <= 60){ // On verifie que la longueur du pseudo <= 60
-            if(strlen($email) <= 100){ // On verifie que la longueur du mail <= 100
-                if(filter_var($email, FILTER_VALIDATE_EMAIL)){ // Si l'email est de la bonne forme
-                    if($password == $password_retype){ // si les deux mdp saisis sont bons
-
-                        $password = password_hash($password, PASSWORD_BCRYPT);
-
-                        $statement = $bdd->prepare('INSERT INTO users (afpa_id, email, firstname, lastname, password) VALUES (:afpa_id, :email, :firstname, :lastname, :password)');
-                        $statement->execute(  ['afpa_id' => $idAfpa,
-                        'firstname' => $firstName,
-                        'lastname' => $lastName,
-                        'email' => $email,
-                        'password' => $password]);
-
-                        // On redirige avec le message de succès
-                        $_SESSION['LOGGED_USER'] = $idAfpa;
-                        header('Location:index.php');
-                    }else header('Location: index.php?action=signup');
-                }else header('Location: index.php?action=signup');
-            }else header('Location: index.php?action=signup');
-        }else header('Location: index.php?action=signup');
-    }else header('Location: index.php?action=signup');
-
-
-
-
+        $idAfpa = htmlspecialchars($input['afpaId']);
+        $firstName = htmlspecialchars($input['firstName']);
+        $lastName = htmlspecialchars($input['lastName']);
+        $email = htmlspecialchars($input['email']);
+        $password = htmlspecialchars($input['password']);
+        $password_retype = htmlspecialchars($input['password_retype']);
         
 
+        $check = $bdd->prepare('SELECT afpa_id, firstname, lastname, email, password FROM users WHERE afpa_id = ?');
+        $check->execute(array($idAfpa));
+        $data = $check->fetch();
+        $row = $check->rowCount();
+
+        // Si la requete renvoie un 0 alors l'utilisateur n'existe pas 
+        if($row !=0){
+            $_SESSION['ERROR_SIGNIN'] = 'Un utilisateur existe déjà, merci d\'en choisir un autre.';
+            $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+            header('Location: index.php?action=signup');
+            exit;
+        }
+
+        // // Si les deux mdp saisis sont différents
+        else if($password != $password_retype){ 
+            $_SESSION['ERROR_SIGNIN'] = 'Vos mots de passe ne correspondent pas.';
+            $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+            header('Location: index.php?action=signup');
+            exit;
+        }
+
+        //     // On verifie que la longueur du pseudo <= 60
+        else if(strlen($idAfpa) <= 60){ 
+            $_SESSION['ERROR_SIGNIN'] = 'Le pseudo existe déjà, merci d\'en choisir un autre.';
+            $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+            header('Location: index.php?action=signup');
+            exit;
+        }
+
+        // // On verifie que la longueur du mail <= 100    
+        else if(strlen($email) > 150){ 
+            $_SESSION['ERROR_SIGNIN'] = 'Le pseudo existe déjà, merci d\'en choisir un autre.';
+            $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+            header('Location: index.php?action=signup');
+            exit;
+        }
+
+        else if (!preg_match(regexPassword, $password)){
+            $_SESSION['ERROR_SIGNIN'] = 'Votre mot de passe n\'est pas valide';
+            $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+            header('Location: ../index.php?action=signin');
+            exit;
+        }
+
+        else{
+            $password = password_hash($password, PASSWORD_BCRYPT);
+            $statement = $bdd->prepare('INSERT INTO users (afpa_id, email, firstname, lastname, password) VALUES (:afpa_id, :email, :firstname, :lastname, :password)');
+            $statement->execute(  ['afpa_id' => $idAfpa,
+            'firstname' => $firstName,
+            'lastname' => $lastName,
+            'email' => $email,
+            'password' => $password]);
+            // On redirige avec le message de succès
+            $_SESSION['LOGGED_USER'] = $idAfpa;
+            header('Location:index.php');
+        }
+
+    }
+    
+    else {
+        $_SESSION['ERROR_SIGNIN'] = 'Un des champs n\'est pas rempli.';
+        $_SESSION['ERROR_SIGNIN_INPUT'] = $input;
+        header('Location: ../index.php?action=signin');
+        exit;
     }
 }
