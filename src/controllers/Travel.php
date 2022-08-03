f5402212bd75cc06ed531b446fea41adb5bbffd4
Aminebouaich <amine.stunteur15@gmail.com>
2022-08-03 16:43:54 +0200

    add chat

--- src/controllers/Travel.php
+++ src/controllers/Travel.php
@@ -1,21 +1,40 @@
-<?php 
+<?php
 
 
 
 function displayTravel($id)
-    {
-        session_start();
-        require_once('../src/pdo/pdo.php');
-        require_once('../src/Repository/TravelRepository.php');
-        require_once('../src/Repository/UserRepository.php');
-        var_dump($id);
+{
+    session_start();
+    require_once('../src/pdo/pdo.php');
+    require_once('../src/Repository/TravelRepository.php');
+    require_once('../src/Repository/UserRepository.php');
+    require_once('../src/Repository/MessageRepository.php');
 
 
-        $travel=getTravelById($id, $bdd);
-        // var_dump($travel);
-        
-        $user = getUserById($travel["user_id"], $bdd);
-        // var_dump($user);
 
-        require('../templates/travel.php');
-    }
+    $travel = getTravelById($id, $bdd);
+    // var_dump($travel);
+
+    $user = getUserById($travel["user_id"], $bdd);
+    $messages = getMessage($id, $bdd);
+
+
+    // var_dump($user);
+
+
+
+    require('../templates/travel.php');
+}
+
+function submitMessage($input)
+{
+    session_start();
+    require_once('../src/Repository/MessageRepository.php');
+    var_dump($input);
+    $content = htmlspecialchars($input['message']);
+    $userId = $input['userId'];
+    $travelId = $input['travelId'];
+    $date = new DateTime();
+    addMessage($content, $userId, $travelId, $date);
+    header('Location: index.php?action=travel&id=' . $travelId);
+}
--- templates/travel.php
+++ templates/travel.php
@@ -3,33 +3,87 @@
 
 
 <?php ob_start(); ?>
+<?php var_dump($travel); ?>
+<?php var_dump($user); ?>
 
 
 <!-- <a href="./index.php?action=travel&id=39">test</a> -->
-<h1></h1>
-<div class="vtl">
-  <div class="event">
-    <p class="date">Départ</p>
-    <p class="txt"><?= $travel["start"] ?></p>
-  </div>
-  <?php
-//   var_dump($travel["list_steps"]);
-  $steps= json_decode($travel["list_steps"]);
-  foreach ($steps as $key => $step) {
-    if ($step != $travel["start"] && $step != $travel["destination"] ){
-        echo '<div class="event">
-        <p class="date">Étape '.$key.'</p>
-        <p class="txt">'.$step.'</p>
-      </div>';
-    }
-  } ?>
-  <div class="event">
-    <p class="date">Arrivée</p>
-    <p class="txt"><?= $travel["destination"] ?></p>
-  </div>
+<div class="container mt-5">
 
+    <div class=" d-flex justify-content-center">
+
+        <div class="col-md-12">
+
+            <div class="card p-3 py-4 d-flex flex-row">
+
+                <div class="text-center p-1">
+                    <img src="https://i.imgur.com/bDLhJiP.jpg" width="100" class="rounded-circle">
+                </div>
+
+                <div class="text-center mt-5">
+                    <h5 class="mt-2 pb-5"><?= $user["firstname"] . " " . $user["lastname"] ?></h5>
+                    <div class="px-4 mt-1">
+                        <p class=" h-5"><?= $user['email'] ?></p>
+                    </div>
+
+                    <div class="buttons">
+
+                        <button class="btn btn-outline-primary ml-3 px-4">Message</button>
+                        <button class="btn btn-primary px-4 ms-5">Contact</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<a href="../index.php?action=booking&id=<?=$travel['travel_id'] ?>" class="btn btn-primary me-2">Réservez</a>
+
+<main class="container-lg py-5 col-lg-4 m-vh-80">
+
+
+
+    <h1><?= $travel['date_start'] ?> </h1>
+    <p class="h4 mt-5">Places disponibles : <?= $travel["seat_available"] ?> <i class="fa-solid fa-user"></i> </p>
+    <div class="vtl">
+        <div class="event">
+            <p class="date">Départ</p>
+            <p class="txt"><?= $travel["start"] ?></p>
+        </div>
+        <?php
+        //   var_dump($travel["list_steps"]);
+        $steps = json_decode($travel["list_steps"]);
+        foreach ($steps as $key => $step) {
+            if ($step != $travel["start"] && $step != $travel["destination"]) {
+                echo '<div class="event">
+                    <p class="date">Étape ' . $key . '</p>
+                    <p class="txt">' . $step . '</p>
+                    </div>';
+            }
+        } ?>
+        <div class="event">
+            <p class="date">Arrivée</p>
+            <p class="txt"><?= $travel["destination"] ?></p>
+        </div>
+
+    </div>
+
+
+    <a href="../index.php?action=booking&id=<?= $travel['travel_id'] ?>" class="btn btn-primary me-2">Réservez</a>
+
+
+    <form class=" d-flex flex-column gap-3" method="POST" action="../index.php?action=submit-message">
+        <label for="inputMessage">Message</label>
+        <input type="text" class="form-control" id="inputMessage" name="message" placeholder="Tapez votre message" required value="">
+        <input type="hidden" name="userId" value='<?= $_SESSION['LOGGED_USER']['userId'] ?>'>
+        <input type="hidden" name="travelId" value='<?= $travel['travel_id'] ?>'>
+
+        <button type=" submit" class="btn btn-primary">Valider</button>
+
+    </form>
+
+</main>
+
+<?php var_dump($messages); ?>
+
 
 
 
