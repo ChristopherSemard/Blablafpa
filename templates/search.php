4a485e0cff3b73364d8643cfb5522d91cf93a5e5
damirdine <damirdine@outlook.fr>
2022-08-03 14:09:25 +0200

    front search

--- templates/search.php
+++ templates/search.php
@@ -3,26 +3,41 @@
 
 <?php ob_start(); ?>
 
-<form method="post">
-<div class="input">
-    <label for="">Depart</label>
-    <input type="text" class="cityAutocomplete" name='start'placeholder="depart">
-</div>
-    <label for="">Arrive</label>
-    <input type="text" class="cityAutocomplete" name='finish' placeholder="arrive">
-</div>
-</div>
-    <label for="">Places</label>
-    <input type="number" class="number" name='seat' placeholder="Nombre de places">
-</div>
-<input type="submit" value="Rechercher">
-</form>
+<main class=" m-vh-80">
 
-<?php if(isset($availableTravel) && count($availableTravel)>0){
-echo('<pre>');
-echo('<pre>');
-var_dump($availableTravel);
-}else{ print('no travail available');}?>
+    <img class="image-background" src="./assets/images/background.jpg" alt="">
+    <div class="form-on-background p-4 rounded">
+        <h2 class="color-secondary">Recherchez un trajet !</h2>
+        <form method="post">
+            <div class="input">
+                <label for="">Départ</label>
+                <?php if (isset($_POST['start'], $_POST['finish'], $_POST['seat'])) : ?>
+                    <input type="text" value="<?= $_POST['start'] ?>" class="cityAutocomplete form-control" name='start' placeholder="Ville de départ">
+                    <label for="">Arrivée</label>
+                    <input type="text" value="<?= $_POST['finish'] ?>" class="cityAutocomplete form-control" name='finish' placeholder="Ville d'arrivée">
+                    <label for="">Places</label>
+                    <input type="number" value="<?= $_POST['seat'] ?>" class="number form-control" name='seat' placeholder="Nombre de places">
+                <?php else : ?>
+                    <input type="text" class="cityAutocomplete form-control" name='start' placeholder="Ville de départ">
+                    <label for="">Arrivée</label>
+                    <input type="text" class="cityAutocomplete form-control" name='finish' placeholder="Ville d'arrivée">
+                    <label for="">Places</label>
+                    <input type="number" class="number form-control" name='seat' placeholder="Nombre de places">
+                <?php endif ?>
+            </div>
+            <button type="submit" class="btn bg-success mt-2">Rechercher</button>
+        </form>
+        <?php if (isset($availableTravel) && count($availableTravel) > 0) {
+            echo ('<pre>');
+            var_dump($availableTravel);
+            echo ('<pre>');
+        } else {
+            print('no travail available');
+        } ?>
+    </div>
+</main>
+
+
 <script src="./assets/js/autocomplete.js"></script>
 
 <?php $content = ob_get_clean(); ?>
