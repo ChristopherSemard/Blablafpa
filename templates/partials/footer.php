bf489076b894c9ff3776c1d81c9adb5874098b18
ChristopherSemard <christophersemard76@gmail.com>
2022-08-04 15:41:07 +0200

    footer

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,6 +1,6 @@
-
 <footer class="container-fluid mt-auto text-center py-4 vh-10">
-    <p>BLABLAFPA</p>
+    <p>Projet <strong>BlablAFPA</strong> d'application web de covoiturage par <strong>Amine BOUAICH, Christopher SEMARD, Damirdine ALI SOILIHI</strong></p>
+    <p>© Copyright BlablAFPA 2022</p>
 </footer>
 
 <!-- JavaScript Bundle with Popper -->
